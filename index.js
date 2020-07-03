09e789496a8957ce9ccf47f6dcd4013850a3d297
imbhargav5 <imbhargav5@gmail.com>
2020-07-03 15:56:55 +0530

    Add delete aws service action

---
+++ index.js
@@ -0,0 +1,46 @@
+const aws = require('aws-sdk');
+const core = require('@actions/core');
+
+async function descaleService(ecs, {cluster, service}){
+    const response = await ecs.updateService({
+        service,
+        cluster,
+        "desiredCount": 0,
+    }).promise();
+    core.info(`Service ${service} is scaled to 0 in cluster ${cluster}`);
+    return response;
+}
+
+async function deleteService(ecs, {cluster, service}){
+    const descaleResponse = await descaleService(ecs, {cluster, service});
+    const response = await ecs.deleteService({
+        service,
+        cluster
+    }).promise();
+    return response;
+}
+
+async function run(){
+    try{
+        const ecs = new aws.ECS({
+            customUserAgent: 'amazon-ecs-deploy-task-definition-for-github-actions'
+        });
+        const cluster = core.getInput("ecs-cluster-name");
+        const serviceName = core.getInput("ecs-service-name");
+        await deleteService(ecs, {
+            cluster,
+            service: serviceName
+        });
+        core.info(`Service ${service} is deleted in cluster ${cluster}`);
+    }  catch(error){
+        core.setFailed(error);
+        core.info(error);
+    }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
