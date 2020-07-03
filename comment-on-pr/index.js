183e1cd17c7da7761a6cf3b0e855502601a16f39
imbhargav5 <imbhargav5@gmail.com>
2020-07-03 18:47:53 +0530

    Add comment on pr action

---
+++ comment-on-pr/index.js
@@ -0,0 +1,37 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+export async function run(){
+  try {
+
+    // Get input parameters.
+    const token = core.getInput('token')
+    const body = core.getInput('body')
+    const prNumber = core.getInput('pr-number')
+    const repository = core.getInput("repository")
+    
+    core.debug(`body: ${body}`)
+    core.debug(`prNumber: ${prNumber}`)
+
+    // Create a GitHub client.
+    const client = new github.GitHub(token)
+
+    // Get owner and repo from
+    const [owner, repo] = repository.split("/");
+
+
+    // Create a comment on PR
+    // https://octokit.github.io/rest.js/#octokit-routes-issues-create-comment
+    const response = await client.issues.createComment({
+      owner,
+      repo,
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      issue_number: prNumber,
+      body
+    })
+    core.debug(`created comment URL: ${response.data.html_url}`)
+    core.setOutput('comment-url', response.data.html_url)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
